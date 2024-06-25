7774819cd9ca746549e509d4883b40bd23cd424c
redondo <riad.reda.fethi@gmail.com>
2024-06-25 13:25:08 +0200

    ajout bg de section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,7 +3,7 @@ import { TypewriterEffectSmoothDemo } from './ui/type-writer/TypewriterEffectSmo
 
 const Section: React.FC = () => {
   return (
-    <section>
+    <section className='w-full bg-blue-200'>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
           <div className="lg:py-24">
