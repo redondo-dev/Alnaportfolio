29d15340c51dabcd716437746fafc8434d673de9
redondo <riad.reda.fethi@gmail.com>
2024-07-19 11:20:51 +0200

    modification nom de fichiert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { SparklesPreview } from "./components/ui/sparkles/sparklesPreview";
 import { AnimatedTooltipPreview } from "./components/ui/info-bulle/AnimatedTooltipPreview";
 import Nav from "./components/Nav";
-import ContactForm from "./components/contactForm";
+import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
 import modalButton from "./components/modalButton";
 import About from "./components/About";
